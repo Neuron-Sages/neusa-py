410ebb18488b59ac620d5876c24e2b4686b7750c
Oleh Kylymnyk <okilimnik@gmail.com>
2023-06-19 12:03:56 +0200

    WIP

--- trade.py
+++ trade.py
@@ -78,13 +78,11 @@ api_key, api_secret, url, ws_url = get_params()
 model = load_learner(fname='model.pkl', cpu=True)
 
 def predict(input):
-    with model.no_bar():
-        prediction, t_index, t_confidence_array = model.predict(get_input_image(input))
-        index = t_index.detach().cpu().numpy()
-        confidence_array = t_confidence_array.detach().cpu().numpy()
-        confidence = confidence_array[index]
-        print(prediction, confidence)
-        return prediction, confidence
+    prediction, t_index, t_confidence_array = model.predict(get_input_image(input))
+    index = t_index.detach().cpu().numpy()
+    confidence_array = t_confidence_array.detach().cpu().numpy()
+    confidence = confidence_array[index]
+    return prediction, confidence
 
 def get_stops(prediction, price):
     stop_loss_shift = int(price / 100.0 * STOP_LOSS)
@@ -112,9 +110,17 @@ def get_new_order_params(prediction, price):
         }
     }
 
-opened_order = {}
+
+start_time_string = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+total_profit_orders_count = 0
+total_loss_orders_count = 0
+opened_order = None
 
 def run_algorithm():
+    global opened_order
+    global total_loss_orders_count
+    global total_profit_orders_count
+    global start_time_string
     input = rest_client.klines(SYMBOL, INTERVAL, limit=RAW_INPUT_SIZE)
     kline = input[-1]
     price = float(kline[4])
@@ -122,16 +128,52 @@ def run_algorithm():
     if confidence >= THRESHOLD and prediction != 'wait':
         new_order_params = get_new_order_params(prediction, price)
         now = datetime.now()
-        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-        print(dt_string, ': I would open a new order with params:', new_order_params)
+        now_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        if opened_order == None:
+            opened_order = new_order_params
+            print("I have opened an order now:",
+                opened_order['metadata']['prediction'])
+        else:
+            old_metadata = opened_order['metadata']
+            old_side = old_metadata['prediction']
+            old_price = old_metadata['price']
+            new_metadata = new_order_params['metadata']
+            new_side = new_metadata['prediction']
+            new_price = new_metadata['price']
+            if old_side != new_side:
+                side = old_side
+                price = new_price
+                if side == 'buy':
+                    if price > old_price:
+                        print('Closing buy order with profit')
+                        total_profit_orders_count = 1 + total_profit_orders_count
+                        opened_order = None
+                    if price <= old_price:
+                        print('Closing buy order with loss')
+                        total_loss_orders_count = 1 + total_loss_orders_count
+                        opened_order = None
+                if side == 'sell':
+                    if price < old_price:
+                        print('Closing sell order with profit')
+                        total_profit_orders_count = 1 + total_profit_orders_count
+                        opened_order = None
+                    if price >= old_price:
+                        print('Closing sell order with loss')
+                        total_loss_orders_count = 1 + total_loss_orders_count
+                        opened_order = None
+                opened_order = new_order_params
+                with open('log.txt', 'w') as f:
+                    print('From ', start_time_string, 'to', now_string, 'total_profit_orders_count=', total_profit_orders_count, 'total_loss_orders_count=', total_loss_orders_count, file=f)
         #response = rest_client.new_order(**new_order_params['binance_order'])
         #print(response)
         #status = response['status']
         #if status == 'FILLED':
         #    opened_order = new_order_params
 
-
 def maybe_close_opened_order(price):
+    global opened_order
+    global total_loss_orders_count
+    global total_profit_orders_count
     metadata = opened_order['metadata']
     side = metadata['prediction']
     stop_loss = metadata['stop_loss']
@@ -139,35 +181,47 @@ def maybe_close_opened_order(price):
     if side == 'buy':
         if price >= stop_profit:
             print('Closing buy order with profit')
-            opened_order = {}
+            total_profit_orders_count = 1 + total_profit_orders_count
+            opened_order = None
         if price <= stop_loss:
             print('Closing buy order with loss')
-            opened_order = {}
+            total_loss_orders_count = 1 + total_loss_orders_count
+            opened_order = None
     if side == 'sell':
         if price <= stop_profit:
             print('Closing sell order with profit')
-            opened_order = {}
+            total_profit_orders_count = 1 + total_profit_orders_count
+            opened_order = None
         if price >= stop_loss:
             print('Closing sell order with loss')
-            opened_order = {}
+            total_loss_orders_count = 1 + total_loss_orders_count
+            opened_order = None
 
 def message_handler(_, message):
+    global opened_order
     parsed = json.loads(message)
     if 'e' in parsed:  # kline subscription message
         k = parsed['k']
+        #if opened_order != None:
+        #    maybe_close_opened_order(float(k['c']))
         if k['x'] == True:  # kline is closed
-            if opened_order != {}:
-                maybe_close_opened_order(float(k['c']))
-            if opened_order == {}:
-                run_algorithm()
+            #if opened_order == None:
+            run_algorithm()
+
+def on_ws_open(socket_manager):
+    print('Subscribed to ', SYMBOL, 'interval=', INTERVAL)
+
+def on_ws_close(socket_manager):
+    print('websocket connection closed, reopening')
+    socket_manager.create_ws_connection()
 
 rest_client = Spot(api_key=api_key, api_secret=api_secret, base_url=url)
-ws_client = SpotWebsocketStreamClient(on_message=message_handler, stream_url=ws_url  # , on_close=on_close
-                                      )
+ws_client = SpotWebsocketStreamClient(
+    on_message=message_handler, stream_url=ws_url, on_open=on_ws_open, on_close=on_ws_close, on_error=on_ws_close)
 def get_balance():
     print(rest_client.account())
 
 ws_client.kline(symbol=SYMBOL, interval=INTERVAL)
 
 
-#python3 trade.py > log.txt
+#python3 trade.py
