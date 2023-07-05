5691d4233872f6717a7fef6cfa514b78a7e728dc
Oleh Kylymnyk <okilimnik@gmail.com>
2023-07-05 11:56:43 +0200

    done selecting indicators

--- script.clj
+++ script.clj
@@ -7,4 +7,7 @@
 
 (def sorted-data (sort-by (comp (juxt :vloss :last-erate) val) (:cols data)))
 
-(pprint (take 10 sorted-data))
+
+(pprint (take 11 sorted-data))
+
+;; TSIs_13_25_13 == wma == t3 == trima
